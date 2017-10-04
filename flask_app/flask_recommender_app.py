036b2a840fa845c775f87c58a8a13078957d2ffa
Daniel Edstrom <danedstrom@Daniels-MacBook-Pro.local>
2017-10-03 20:28:45 -0600

    added 3-d plot page

--- flask_app/flask_recommender_app.py
+++ flask_app/flask_recommender_app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, json, jsonify
 import pickle
 import pandas as pd
+import numpy as np
+
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+#The line below will let you run this offline.
+from plotly.offline import download_plotlyjs,plot, iplot
+import pickle
 
 from job_classifier import JobRecommender
 
@@ -16,6 +24,9 @@ with open('../data/jobs_similarity_matrix2.pkl', 'rb') as m:
 
 with open('../data/job_details_dict.pkl', 'rb') as det:
     details_dict = pickle.load(det)
+
+with open('../data/descrip_nmf_matrix.pkl', 'rb') as f:
+    W = pickle.load(f)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -54,12 +65,22 @@ def predict():
 
     return render_template('predict.html', job=jobtitle, details=details_list, states=states, jobids = ids)
 
-@app.route('/get_jobs_in_selected_states/', methods=['GET'])
+@app.route('/get_jobs_in_selected_states', methods=['POST','GET'])
 def get_jobs_in_selected_states():
     states = request.args.get('states')
     data = model.get_subset_jobs_list(states, details_dict)
     new_dict = {"id": data[0], "title": data[1]}
-    return jsonify(new_dict)
+    return render_template('get_jobs_in_selected_states.html', test=new_dict, states=states)
+    # return jsonify(new_dict)
+
+@app.route('/3dplot_template', methods=['POST','GET'])
+def plot_template():
+    x = list(W[:,1].flatten())
+    y = list(W[:,9].flatten())
+    z = list(W[:,6].flatten())
+    c = list(np.argmax(W, axis=1))
+
+    return render_template('3dplot_template.html', x=json.dumps(x), y=json.dumps(y), z=json.dumps(z), c=c)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
